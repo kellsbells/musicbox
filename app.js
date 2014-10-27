91bb8c383ce5ab482792834b4b6e20bd74377847
kellsbells <kelleeballard@hotmail.com>
2014-10-27 13:49:03 -0600

    Makes the birds sing

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 
 $(document).ready(function() {
+
     
+    var csomethingNote = document.getElementById('csomethingAudio');
+    $('.csomething').mouseover(function(){
+        csomethingNote.currentTime = 0;
+        csomethingNote.play();
+    });
+
+
     var cNote = document.getElementById('cAudio');
-    $('#c').mouseover(function(){
+    $('.c').mouseover(function(){
         cNote.currentTime = 0;
         cNote.play();
     });
@@ -10,15 +18,25 @@ $(document).ready(function() {
     
     var dNote = document.getElementById('dAudio');
     
-    $('#d').mouseover(function(){
+    $('.d').mouseover(function(){
         dNote.currentTime = 0;
         dNote.play();
     });
 
+
+    var ebNote = document.getElementById('ebAudio');
+    
+    $('.eb').mouseover(function(){
+        ebNote.currentTime = 0;
+        ebNote.play();
+    });
+
+
+
     
     var eNote = document.getElementById('eAudio');
     
-    $('#e').mouseover(function(){
+    $('.e').mouseover(function(){
         eNote.currentTime = 0;
         eNote.play();
     });
@@ -27,23 +45,39 @@ $(document).ready(function() {
     
     var fNote = document.getElementById('fAudio');
     
-    $('#f').mouseover(function(){
+    $('.f').mouseover(function(){
         fNote.currentTime = 0;
         fNote.play();
+    });
+
+
+    var fsomethingNote = document.getElementById('fsomethingAudio');
+    
+    $('.fsomething').mouseover(function(){
+        fsomethingNote.currentTime = 0;
+        fsomethingNote.play();
     });
 
     
     var gNote = document.getElementById('gAudio');
     
-    $('#g').mouseover(function(){
+    $('.g').mouseover(function(){
         gNote.currentTime = 0;
         gNote.play();
+    });
+
+
+    var gsomethingNote = document.getElementById('gsomethingAudio');
+    
+    $('.gsomething').mouseover(function(){
+        gsomethingNote.currentTime = 0;
+        gsomethingNote.play();
     });
 
     
     var aNote = document.getElementById('aAudio');
     
-    $('#a').mouseover(function(){
+    $('.a').mouseover(function(){
         aNote.currentTime = 0;
         aNote.play();
     });
@@ -51,9 +85,17 @@ $(document).ready(function() {
     
     var bNote = document.getElementById('bAudio');
     
-    $('#b').mouseover(function(){
+    $('.b').mouseover(function(){
         bNote.currentTime = 0;
         bNote.play();
     });
+
+    var bbNote = document.getElementById('bbAudio');
     
+    $('.bb').mouseover(function(){
+        bbNote.currentTime = 0;
+        bbNote.play();
+    });
+
+
 });
